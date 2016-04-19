465a0f46f6adf2ae26ccf746e1e77a6bc09ca08e
pluralparallel <pluralparallel@gmail.com>
2016-04-19 19:07:24 +0200

    update build.sh

--- build.sh
+++ build.sh
@@ -1,15 +1,12 @@
 ls
 echo $PWD
-git checkout source
-git clone https://github.com/onur/medius.git
-pelican-themes -i medius/
-pelican content
+pelican-themes -i themes/medius/
+pelican content -o /tmp/output
 git config --local user.name pluralparallel
 git config --local user.email pluralparallel@circleci.com
 git checkout master
 git reset --hard 7ed15ee136
 git push origin master --force
-mv output /tmp
 mv LICENSE /tmp
 rm -rf *
 mv /tmp/LICENSE .
